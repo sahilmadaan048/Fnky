6dae1a39fb35b5fa337e412e3f4fca5f39e193fa
Sahil Madaan <madaan.sahil27@gmail.com>
2025-04-13 13:12:29 +0530

    continue

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,6 +6,7 @@ impl Interpreter {
     pub fn new() -> Self {
         Self {}
     }
+    //doing this for fun
     pub fn interpret(&mut self, expr: Expr) -> Result<LiteralValue, String> {
         expr.evaluate()
     }
